bfe1d7c30c9c8f96db4bf8eeee211b2f980857a1
Artur Shamsutdinov <a.shamsutdinov@icloud.com>
2022-08-20 03:13:22 +0500

    First sprite load

--- lib/game.dart
+++ lib/game.dart
@@ -1,3 +1,15 @@
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 
-class RoadCopGame extends FlameGame {}
+class RoadCopGame extends FlameGame {
+  @override
+  Future<void>? onLoad() async {
+    await images.load('player.png');
+    SpriteComponent player = SpriteComponent(
+        sprite: Sprite(images.fromCache('player.png')),
+        position: Vector2(0, 0));
+
+    add(player);
+    return super.onLoad();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:roadcop/game.dart';
 
 void main() {
+  // Game view settings
+  WidgetsFlutterBinding.ensureInitialized();
+  Flame.device.fullScreen();
+
+  // Game launch
   final game = RoadCopGame();
   runApp(GameWidget(game: game));
 }
