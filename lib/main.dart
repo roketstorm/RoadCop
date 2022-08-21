997e887a543c661380c2187bdd3c107d585659de
Artur Shamsutdinov <a.shamsutdinov@icloud.com>
2022-08-21 10:04:56 +0500

    Added splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flame/flame.dart';
+import 'package:flame_splash_screen/flame_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:roadcop/screens/main_menu_screen.dart';
 
@@ -19,7 +20,36 @@ void main() {
   // ));
 
   runApp(MaterialApp(
-    home: const MainMenuScreen(),
+    // home: const MainMenuScreen(),
+    home: FlameSplashScreen(
+      theme: FlameSplashTheme(
+        backgroundDecoration: const BoxDecoration(color: Color(0xFF000000)),
+        logoBuilder: (context) =>
+            LayoutBuilder(builder: (context, constraints) {
+          return FractionalTranslation(
+            translation: const Offset(0, -0.25),
+            child: ConstrainedBox(
+              constraints: BoxConstraints.loose(const Size(300, 300)),
+              child: Image.asset('assets/images/flame_logo_green.png'),
+            ),
+          );
+        }),
+      ),
+      showAfter: (context) => const Text(
+        'RoketStorm',
+        style: TextStyle(
+          fontFamily: 'Nero',
+          color: Color.fromRGBO(0, 200, 0, 1.0),
+          fontSize: 42,
+        ),
+      ),
+      onFinish: (context) => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const MainMenuScreen(),
+        ),
+      ),
+    ),
     theme: ThemeData(
       backgroundColor: Colors.black,
       scaffoldBackgroundColor: Colors.black,
